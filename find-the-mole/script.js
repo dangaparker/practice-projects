fa2b269677e706a10d93258b9e97d82d43da0782
Martin92571 <zegrizo@gmail.com>
2018-06-01 11:24:57 -0700

    jquery

--- find-the-mole/script.js
+++ find-the-mole/script.js
@@ -6,15 +6,17 @@ function startGame(){
   })
    function randomMole(){
        $(".cover").css("display","block");
-       var mathRandom= Math.floor(Math.random() *3);
+       var mathRandom= Math.floor(Math.random() *3)+1;
        var moleString=".mole"+mathRandom;
        var cover=".cover"+mathRandom;
        $(cover).css("display","none");
-       if(player==5)
+       if(player==5){
        youWin();
        clearInterval(startInt);
         }
-    var startInt=setInterval(randomMole,500);
+   
+}
+var startInt=setInterval(randomMole,500);
 }
 
 function youWin() {
